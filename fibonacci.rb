1a876332f1d5401249fb1b2b991df063f9c101c5
Philipe Godoy <philipe@sorrydave.com.br>
2014-12-08 11:54:20 -0200

    done

--- fibonacci.rb
+++ fibonacci.rb
@@ -1,8 +1,22 @@
 def get_fibonacci( number )
+	factor1 = 0
+	factor2 = 1
+	factor3 = factor1 + factor2
+	i = 3
+	puts "#{factor1}"
+	puts "#{factor2}"
+	until i > number		
+		
+		factor1 = factor2
+		factor2 = factor3
+		factor3 = factor1 + factor2
 
+		puts "#{factor3} " 
+		i = i + 1
+	end
 
 end
 
-print "what number of fibonacci do you want ?"
+print "what number of fibonacci do you want ? "
 desire_number = gets.strip.to_i
 get_fibonacci(desire_number)
